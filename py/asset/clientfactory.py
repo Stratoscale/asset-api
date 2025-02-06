586564bd16fe4e96b711ef3fc780b95a04cd4738
IliaFeldgun <ilia.feldgun@zadarastorage.com>
2025-02-06 03:24:27 +0200

    RD-1978 get rid of pyyaml legacy code

--- py/asset/clientfactory.py
+++ py/asset/clientfactory.py
@@ -11,5 +11,5 @@ _VAR_NAME = "ASSET_CONFIG_FILE"
 def factory():
     configFile = os.environ.get(_VAR_NAME, _CONFIG_FILE_NAME)
     with open(configFile) as f:
-        config.Config(yaml.load(f))
+        config.Config(yaml.safe_load(f))
     return client.Client(config.config.provider())
